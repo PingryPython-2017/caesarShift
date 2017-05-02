74c493630932fac870338d49b3569f21d40c0b29
Dilan Bhat <dbhat2020@pingry.org>
2017-05-02 12:43:07 -0400

    Finished

--- caesar.py
+++ caesar.py
@@ -1,10 +1,24 @@
+
 def caesar_shift(word,shift):
-	output = ""
-	if len(output) == len(word):
-		return output
-	first=word[0]
-	ascii=ord(first)
-	asciishift = chr(ascii + shift)
-	output = asciishift + caesar_shift(str(word[1:]),shift)
-	
-print (caesar_shift("hi",1))
+	"""Takes in a string and a shift amount and shifts the word that many ascii characters"""
+
+	asciimin = 32
+	asciimax = 126
+	# Terminating case
+	if len(word) == 0:
+		return word
+	# Takes the first letter of the word
+	letter = word[:1]
+	# If it will shift above the ascii numbers for letters
+	if ord(letter) + shift >= asciimax:
+		wrap =  asciimax - ord(letter)
+		wrap = shift - wrap
+		wrap += asciimin - 1
+		
+	# If it won't shift above
+	else:
+		wrap = ord(letter) + shift
+	return chr(wrap) + caesar_shift(word[1:],shift)
+
+
+
